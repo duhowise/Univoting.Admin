37137df68eed5a0526e6f58c356cad2b2ab15c90
duho wise <wduho@hotmail.com>
2019-02-09 14:41:57 +0000

    Add project files.

---
+++ Univoting.Admin/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Univoting.Admin.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace Univoting.Admin
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
